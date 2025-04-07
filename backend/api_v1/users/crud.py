99ee42481aeb1834be9c1fd051a01cf65d751c86
a.dolgov <dolgov_am@mail.ru>
2025-04-07 12:36:23 +0300

    add crud

---
+++ backend/api_v1/users/crud.py
@@ -0,0 +1,23 @@
+from sqlalchemy import select
+from sqlalchemy.engine import Result
+from sqlalchemy.ext.asyncio import AsyncSession
+from core.models import User
+from watchfiles import awatch
+
+from api_v1.users.schemas import UserCreate
+
+
+async def get_users(session: AsyncSession):
+    stmt = select(User).order_by(User.id)
+    result: Result = await session.execute(stmt)
+    users = result.scalars().all()
+    return list(users)
+
+async def get_user(session: AsyncSession, user_id: int):
+    return await session.get(User, user_id)
+
+async def create_user(session: AsyncSession, user_in: UserCreate):
+    user = User(**user_in.model_dump())
+    session.add(user)
+    await session.commit()
+    return user
