4aace8a8625b8c52abe95218bfb6f2caadbd6d14
a.dolgov <dolgov_am@mail.ru>
2025-04-07 17:36:13 +0300

    finaly changes

--- backend/api_v1/users/schemas.py
+++ backend/api_v1/users/schemas.py
@@ -5,6 +5,9 @@ class UserBase(BaseModel):
     username: str
     password: str
     email: str
+    role: str
+    first_name: str
+    last_name: str
 
 class UserCreate(UserBase):
     pass
@@ -16,6 +19,9 @@ class UserUpdatePartial(UserBase):
     username: str | None = None
     password: str | None = None
     email: str | None = None
+    role: str | None = None
+    first_name: str | None = None
+    last_name: str | None = None
 
 class UserSchema(UserBase):
     model_config = ConfigDict(from_attributes=True)
--- backend/core/models/user.py
+++ backend/core/models/user.py
@@ -5,3 +5,6 @@ class User(Base):
     username: Mapped[str]
     password: Mapped[str]
     email: Mapped[str]
+    role: Mapped[str]
+    first_name: Mapped[str]
+    last_name: Mapped[str]
